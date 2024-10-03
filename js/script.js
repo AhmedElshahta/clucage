8a04157f4ac0cdbd7e0a658222e23ebfd374c811
AhmedElshahta <141634198+AhmedElshahta@users.noreply.github.com>
2024-10-03 22:44:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function calculateAge() {
+    const day = parseInt(document.getElementById('day').value);
+    const month = parseInt(document.getElementById('month').value);
+    const year = parseInt(document.getElementById('year').value);
+
+    if (!day || !month || !year) {
+        alert("Please enter a valid date of birth.");
+        return;
+    }
+
+    const birthDate = new Date(year, month - 1, day);
+    const today = new Date();
+
+    let ageYears = today.getFullYear() - birthDate.getFullYear();
+    let ageMonths = today.getMonth() - birthDate.getMonth();
+    let ageDays = today.getDate() - birthDate.getDate();
+
+    if (ageDays < 0) {
+        ageMonths--;
+        ageDays += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+
+    if (ageMonths < 0) {
+        ageYears--;
+        ageMonths += 12;
+    }
+
+    document.getElementById('age').textContent = "years" + ageYears;
+    document.getElementById('mongth').textContent =  "months" + ageMonths;
+    document.getElementById('days').textContent = "days" + ageDays;
+
+}
